c09b27c0c6f1106290bb7d1f8a367cd3082bb5fb
NyyWlTekk <23penny23@gmail.com>
2026-06-20 20:26:07 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
-make clean                    
+# 1. Přesun do složky se skriptem
+cd "$(dirname "$0")"
+
+# 2. Vyčištění starých souborů
+echo "--- Čistím staré build soubory ---"
+make clean > /dev/null 2>&1
+rm -f Makefile
+
+# 3. Konfigurace projektu (qmake)
+echo "--- Spouštím qmake ---"
 qmake
-make
+
+# 4. Kompilace (make)
+# Použijeme parametr -j$(nproc), který využije všechna jádra procesoru pro rychlejší build
+echo "--- Kompiluji projekt ---"
+if make -j$(nproc); then
+    echo ""
+    echo "------------------------------------------"
+    echo "  BUILD ÚSPĚŠNĚ DOKONČEN!"
+    echo "------------------------------------------"
+else
+    echo ""
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "  BUILD SELHAL! Zkontroluj chyby výše."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 1
+fi
